e08f41a0010dc4fbcdde60c42384307f4fb0466b
nicorr <nicolas@mobinlife.com>
2015-01-13 14:26:25 +0100

    FIRST PARALLEL VERSION !

--- app/src/main/java/com/forestwave/pdc8g1/forestwave/StartActivity.java
+++ app/src/main/java/com/forestwave/pdc8g1/forestwave/StartActivity.java
@@ -3,14 +3,27 @@ package com.forestwave.pdc8g1.forestwave;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 
 public class StartActivity extends Activity {
@@ -25,23 +38,111 @@ public class StartActivity extends Activity {
                     .commit();
         }
 
-        // Premiers sons
-        MediaPlayer mp1 = MediaPlayer.create(this, R.raw.guitar);
-        MediaPlayer mp2 = MediaPlayer.create(this, R.raw.mallet);
-        MediaPlayer mp3 = MediaPlayer.create(this, R.raw.flute_piano);
-        MediaPlayer mp4 = MediaPlayer.create(this, R.raw.rhodes);
-        MediaPlayer mp5 = MediaPlayer.create(this, R.raw.pad_synth);
-        mp1.setLooping(true);
-        mp2.setLooping(true);
-        mp3.setLooping(true);
-        mp4.setLooping(true);
-        mp5.setLooping(true);
-        mp1.start();
-        mp2.start();
-        mp3.start();
-        mp4.start();
-        mp5.start();
+        AudioTrackSoundPlayer audioTrackSoundPlayer = new AudioTrackSoundPlayer(this);
+        audioTrackSoundPlayer.playNote("ds");
+        audioTrackSoundPlayer.playNote("clarinet");
+
     }
+
+    public class AudioTrackSoundPlayer {
+        private HashMap<String, PlayThread> threadMap = null;
+        private Context context;
+        public static final String TAG = "AudioTrackSoundPlayer";
+
+        public AudioTrackSoundPlayer(Context context) {
+            this.context = context;
+            threadMap = new HashMap<String, PlayThread>();
+        }
+
+        public void playNote(String note)
+        {
+            if (!isNotePlaying(note))
+            {
+                PlayThread thread = new PlayThread(note);
+                thread.start();
+                threadMap.put(note, thread);
+
+            }
+        }
+
+        public void stopNote(String note)
+        {
+            PlayThread thread = threadMap.get(note);
+            if (thread != null)
+            {
+                thread.requestStop();
+                threadMap.remove(note);
+            }
+        }
+
+        public boolean isNotePlaying(String note)
+        {
+            return threadMap.containsKey(note);
+        }
+
+        private class PlayThread extends Thread {
+            String note;
+            boolean stop = false;
+            AudioTrack audioTrack = null;
+
+            public PlayThread(String note) {
+                super();
+                this.note = note;
+            }
+
+            public void run() {
+                try {
+                    String path = note + ".wav";
+
+                    AssetManager assetManager = context.getAssets();
+                    AssetFileDescriptor ad = assetManager.openFd(path);
+                    long fileSize = ad.getLength();
+                    int bufferSize = 16000;
+                    byte[] buffer = new byte[bufferSize];
+
+                    audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+
+                    Log.d(TAG, "MIN BUFFER SIZE " + audioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT));
+
+                    audioTrack.play();
+
+                    InputStream audioStream = null;
+
+                    int headerOffset = 0x2C;
+                    long bytesWritten = 0;
+                    int bytesRead = 0;
+
+                    while (!stop) // loop sound
+                    {
+                        audioStream = assetManager.open(path);
+                        bytesWritten = 0;
+                        bytesRead = 0;
+                        Log.d(TAG, "in loop " + path);
+
+                        audioStream.read(buffer, 0, headerOffset);
+
+                        // read until end of file
+                        while (!stop && bytesWritten < fileSize - headerOffset) {
+                            bytesRead = audioStream.read(buffer, 0, bufferSize);
+                            bytesWritten += audioTrack.write(buffer, 0, bytesRead);
+                            //Log.d(TAG, "bytesRead" +bytesRead+"bytesWritten" +bytesWritten);
+                        }
+                    }
+
+                    audioTrack.stop();
+                    audioTrack.release();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            public synchronized void requestStop()
+            {
+                stop = true;
+            }
+        }
+    }
+
 
 
     @Override
