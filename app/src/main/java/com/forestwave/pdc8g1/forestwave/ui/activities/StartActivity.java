7fb81515dcbcc2fb5da9eee9d7d569b21fb7346f
Sylvain Abadie <sylvain.abadie@insa-lyon.fr>
2015-01-26 17:33:26 +0100

    changed the icon

--- app/src/main/java/com/forestwave/pdc8g1/forestwave/ui/activities/StartActivity.java
+++ app/src/main/java/com/forestwave/pdc8g1/forestwave/ui/activities/StartActivity.java
@@ -244,8 +244,8 @@ public class StartActivity extends Activity implements OnClickListener, OnEditor
     private void startAudio() {
         String name = getResources().getString(R.string.app_name);
         try {
-            pdService.initAudio(-1, -1, -1, -1);   // negative values will be replaced with defaults/preferences
-            pdService.startAudio(new Intent(this, StartActivity.class), R.drawable.icon, name, "Return to " + name + ".");
+            pdService.initAudio(44100, -1, -1, -1);   // negative values will be replaced with defaults/preferences
+            pdService.startAudio(new Intent(this, StartActivity.class), R.drawable.ic_launcher, name, "Return to " + name + ".");
         } catch (IOException e) {
             toast(e.toString());
         }
