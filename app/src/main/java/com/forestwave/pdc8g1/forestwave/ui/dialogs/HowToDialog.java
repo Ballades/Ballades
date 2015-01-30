078b7880af2e54a7dc609ec65eeb735dfdee6856
nicorr <nicolas@mobinlife.com>
2015-01-30 15:51:36 +0100

    Ajout des meuns et refonte visuelle

--- app/src/main/java/com/forestwave/pdc8g1/forestwave/ui/dialogs/HowToDialog.java
+++ app/src/main/java/com/forestwave/pdc8g1/forestwave/ui/dialogs/HowToDialog.java
@@ -8,17 +8,17 @@ import android.os.Bundle;
 
 import com.forestwave.pdc8g1.forestwave.R;
 
-public class AboutDialog extends DialogFragment {
+public class HowToDialog extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(R.string.about_dialog_content)
+            builder.setMessage(R.string.how_to_dialog_content)
                     .setPositiveButton(R.string.about_dialog_validate, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                         }
                     })
-                    .setTitle(R.string.about_dialog_title);
+                    .setTitle(R.string.how_to_dialog_title);
 
             // Create the AlertDialog object and return it
             return builder.create();
